b41f708b2b0f3dcfb828aea5f5fd83b8c5559735
M4CLEM <aratanmarkclemence@gmail.com>
2023-04-05 12:04:21 +0800

    as

---
+++ library/src/main/java/com/mycompany/library/Library.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.library;
+
+/**
+ *
+ * @author Clemence
+ */
+
+
+public class Library {
+
+    public static void main(String[] args) {
+
+    }
+}
