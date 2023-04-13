4aab420e4cb4f5e550d3670d959c30d2d8dca2df
Scrappyz <ghostmic3000@gmail.com>
2023-04-13 22:00:22 +0800

    Changed Library.java to Main.java

---
+++ src/main/java/com/mycompany/library/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.library;
+
+import com.mycompany.library.jFrame.Log_In;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Clemence
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        Log_In.main();
+        
+    }
+}
