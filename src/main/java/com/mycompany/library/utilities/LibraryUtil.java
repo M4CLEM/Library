b740444d990380cf508b54617e2818cb2bbbbd12
Scrappyz <ghostmic3000@gmail.com>
2023-05-12 17:10:29 +0800

    Get date utilities

--- src/main/java/com/mycompany/library/utilities/LibraryUtil.java
+++ src/main/java/com/mycompany/library/utilities/LibraryUtil.java
@@ -3,22 +3,21 @@ package com.mycompany.library.utilities;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import com.mycompany.library.Database;
 import com.mysql.cj.xdevapi.Result;
 
 public class LibraryUtil {
     
-    public static int getUserId(String first_name, String middle_name, String last_name, String email)
+    public static int getUserId(final String email)
     {
         int id = 0;
         try {
             Connection con = DriverManager.getConnection(Database.getUrl(), Database.getUsername(), Database.getPassword());
-            PreparedStatement stat = con.prepareStatement("SELECT user_id FROM users WHERE first_name = ? AND middle_name = ? AND last_name = ? AND email = ?");
-            stat.setString(1, first_name);
-            stat.setString(2, middle_name);
-            stat.setString(3, last_name);
-            stat.setString(4, email);
+            PreparedStatement stat = con.prepareStatement("SELECT user_id FROM users WHERE email = ?");
+            stat.setString(1, email);
             ResultSet result = stat.executeQuery();
             if(result.next()) {
                 id = result.getInt(1);
@@ -83,5 +82,51 @@ public class LibraryUtil {
             e.printStackTrace();
         }
         return exist;
+    }
+
+    public static boolean userIdExists(final String id)
+    {
+        boolean exist = false;
+        try {
+            Connection con = DriverManager.getConnection(Database.getUrl(), Database.getUsername(), Database.getPassword());
+            PreparedStatement stat = con.prepareStatement("SELECT user_id FROM users WHERE user_id = ?");
+            stat.setString(1, id);
+            ResultSet rs = stat.executeQuery();
+            if(rs.next()) {
+                exist = true;
+            }
+            rs.close();
+            stat.close();
+            con.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return exist;
+    }
+
+    public static boolean userExists(final String email)
+    {
+        return getUserId(email) > 0;
+    }
+
+    public static String getDatetimeNow()
+    {
+        Date currentDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(currentDate);
+    }
+
+    public static String getAddedDatetime(final String date, int days)
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        try {
+            cal.setTime(formatter.parse(date));
+        } catch(ParseException e) {
+            e.printStackTrace();
+        }
+        cal.add(Calendar.DAY_OF_MONTH, days);
+        String new_date = formatter.format(cal.getTime());
+        return new_date;
     }
 }
