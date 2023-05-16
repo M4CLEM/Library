26cd44a04790e4e5014f8185443e7a1faa250587
Scrappyz <ghostmic3000@gmail.com>
2023-05-16 12:56:07 +0800

    Changed some text

--- src/main/java/com/mycompany/library/jFrame/Records.java
+++ src/main/java/com/mycompany/library/jFrame/Records.java
@@ -86,7 +86,7 @@ public class Records extends javax.swing.JFrame {
         btnReturn.setBackground(new java.awt.Color(11, 50, 69));
         btnReturn.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         btnReturn.setForeground(new java.awt.Color(255, 255, 255));
-        btnReturn.setText("Register");
+        btnReturn.setText("Returned");
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
